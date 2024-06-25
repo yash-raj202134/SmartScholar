69a4d57984e9ff38fded3c254b694fa007216d19
yash-raj202134 <yashraj3376@gmail.com>
2024-06-25 20:41:19 +0530

    main file

--- main.py
+++ main.py
@@ -10,28 +10,31 @@ from src.vectorization import vectorization
 from src.model_trainer import train_model,plot_loss
 from src.model_evaluation import evaluate_model,predict_category,invert_multi_hot
 from src.recommendation import sentence_tnfs,print_embeddings,recommendation
-# data = DataIngestion()
-# file_path = data.download_datasets()
-# data.extract_datasets(file_path)
 
+# Data ingestion stage:
+
+data = DataIngestion()
+file_path = data.download_datasets()
+data.extract_datasets(file_path)
+
+# Preprocessing stage:
 arxiv_data = pd.read_csv("dataset/arxiv_data_210930-054931.csv")
-
 train, validation, test, vocab_size, lookup = preprocess(arxiv_data = arxiv_data)
 
+# Vectorization stage:
 train,validation,test = vectorization(train,test,validation,vocabulary_size=vocab_size)
 
+# Model training stage:
 history = train_model(train_dataset=train,validation_dataset=validation,test_dataset=test,lookup=lookup)
-
 plot_loss(history=history,item="loss",save_path="models")
 plot_loss(history=history,item="binary_accuracy",save_path="models")
 
+# Model evaluation:
 model = keras.models.load_model("models/model.h5")
-# Load the configuration of the text vectorizer
-with open("models/text_vectorizer_config.pkl", "rb") as f:
+with open("models/text_vectorizer_config.pkl", "rb") as f: # Load the configuration of the text vectorizer
     saved_text_vectorizer_config = pickle.load(f)
 
-# Create a new TextVectorization layer with the saved configuration
-text_vectorizer = TextVectorization.from_config(saved_text_vectorizer_config)
+text_vectorizer = TextVectorization.from_config(saved_text_vectorizer_config) # Create a new TextVectorization layer with the saved configuration
 result = evaluate_model(model=model,test_dataset=test,validation_dataset=validation)
 
 # Example usage
@@ -54,7 +57,6 @@ the potential to improve the discriminability of learned graph\nrepresentations.
 predicted_categories = predict_category(new_abstract,model, text_vectorizer, invert_multi_hot)
 print("Predicted Categories:", predicted_categories)
 
-
 # Example usage
 new_abstract = '''Deep networks and decision forests (such as random forests and gradient\nboosted trees)
 are the leading machine learning methods for structured and\ntabular data, respectively. Many papers have empirically 
@@ -75,7 +77,6 @@ predicted_categories = predict_category(new_abstract,model,text_vectorizer, inve
 print("Predicted Categories:", predicted_categories)
 
 
-
 # recommendations
 
 arxiv_data_copy = arxiv_data.copy()
@@ -88,7 +89,6 @@ pd.set_option('display.max_colwidth', None)
 
 rec_model, embeddings, sentences = sentence_tnfs(arxiv_data= arxiv_data_copy)
 print_embeddings(sentences=sentences,embeddings= embeddings)
-
 
 # exampel usage 1: (use this paper as input (Attention is All you Need))
 input_paper = input("Enter the title of any paper you like")
@@ -104,7 +104,6 @@ print("=============================================")
 for paper in recommend_papers:
     print(paper)
 
-
 # exampel usage 2: (use this paper as input (BERT: Pre-training of Deep Bidirectional 
 # Transformers for Language Understanding))
 input_paper = input("Enter the title of any paper you like")
@@ -119,7 +118,6 @@ print("We recommend to read this paper............")
 print("=============================================")
 for paper in recommend_papers:
     print(paper)
-
 
 # exampel usage 3: (use this paper as input (Review of deep learning: concepts,
 #  CNN architectures, challenges, applications, future directions))

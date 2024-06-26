df0d8a909d469e32882ef8b48d416a73745fa0df
yash-raj202134 <yashraj3376@gmail.com>
2024-06-26 12:57:56 +0530

    utils update

--- utils/commons.py
+++ utils/commons.py
@@ -1,6 +1,15 @@
 import tensorflow as tf
+import numpy as np
 
-def make_dataset(dataframe, is_train=True):
+
+def make_dataset(dataframe,batch_size,lookup,is_train=True):
+    """
+        In summary, the make_dataset function is designed to create a 
+        dataset suitable for training a model. It takes a dataframe as input, 
+        assumes it has "abstracts" and "terms" columns, and creates a dataset of 
+        batches where each batch consists of abstract 
+        sequences and their corresponding binarized label sequences. 
+        """
     # creating sequences of labesls
     labels = tf.ragged.constant(dataframe["terms"].values)
     #This line uses the previously defined lookup layer to convert the ragged tensor of labels into a binarized representation. The resulting label_binarized is a NumPy array.
@@ -11,10 +20,21 @@ def make_dataset(dataframe, is_train=True):
     dataset = dataset.shuffle(batch_size * 10) if is_train else dataset
     return dataset.batch(batch_size)
 
-"""
-In summary, the make_dataset function is designed to create a 
-dataset suitable for training a model. It takes a dataframe as input, 
-assumes it has "abstracts" and "terms" columns, and creates a dataset of 
-batches where each batch consists of abstract 
-sequences and their corresponding binarized label sequences. 
-"""
+
+def invert_multi_hot(encoded_labels,loaded_vocab):
+    """Reverse a single multi-hot encoded label to a tuple of vocab terms."""
+    hot_indices = np.argwhere(encoded_labels == 1.0)[..., 0]
+    return np.take(loaded_vocab, hot_indices)
+
+
+def predict_category(abstract, model, vectorizer, label_lookup):
+    # Preprocess the abstract using the loaded text vectorizer
+    preprocessed_abstract = vectorizer([abstract])
+
+    # Make predictions using the loaded model
+    predictions = model.predict(preprocessed_abstract)
+
+    # Convert predictions to human-readable labels
+    predicted_labels = label_lookup(np.round(predictions).astype(int)[0])
+
+    return predicted_labels
